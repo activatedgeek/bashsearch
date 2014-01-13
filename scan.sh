4a44835ff801b07ed541c7ba047c4dcd3034668e
Sanyam Kapoor <ksanyam18@yahoo.com>
2014-01-13 17:57:26 +0530

    Indexing file into "index.txt"

--- scan.sh
+++ scan.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function checkText {
+	echo "$ftype" | grep "ASCII text"
+}
+
+function checkDir {
+	echo "$ftype" | grep "directory"
+}
+
 function populate {
 	#populate current directory list
 	list=$(ls "$@")
@@ -18,10 +26,10 @@ function populate {
 		echo -n -e "$line\t" >> index.txt
 		mod_timestamp=$(stat --format=%y "$@""/""$line")
 		#timestamp
-		echo -n -e "${mod_timestamp:0: -4}" >> index.txt
+		echo -n -e "${mod_timestamp:0: -16}\t" >> index.txt
 
 		#get filetype
-		ftype=$(file "$@""/""$line")
+		ftype="$(file "$@""/""$line")"
 		if [[ ${ftype: -9} = "directory" ]]; then
 			echo -n -e "d\t" >> index.txt
 		else
@@ -29,16 +37,20 @@ function populate {
 		fi
 
 		#get file size
-		echo -n -e "$(stat --format=%s "$@""/""$line")\t" >> index.txt
+		res="$(stat --format=%s "$@""/""$line")"
+		res=$((res/1024))
+		echo -n -e "$res\t" >> index.txt
 		#check for text file
-		if [[ ${ftype: -5} = "ASCII" ]]; then
+		checkAscii=$(checkText "$ftype")
+		if [[ -n "$checkAscii" ]]; then
 			echo "t" >> index.txt
 		else
 			echo "n" >> index.txt
 		fi
 
 		#recursive step
-		if [[ ${ftype: -9} = "directory" ]]; then
+		ifdir=$(checkDir "$ftype")
+		if [[ -n "$ifdir" ]]; then
 			populate "$@""/""$line"
 		fi
 	done <<< "$list"
@@ -51,6 +63,7 @@ rm index.txt > /dev/null 2>&1
 dir="$@"
 if [[ "$dir" = "" ]]; then
 	dir=$(pwd)
-	echo "No Path indicated. Indexing for current working directory..."
+	echo "Indexing for current working directory..."
 fi
 populate "$dir"
+echo "Indexing done in \"$dir\""
